10a1c63d1a38f7748becf9175e4db0f9480de4b2
mnk1910 <mnk1910@local>
2017-11-13 14:44:54 +0100

    This is my third commit. I am finished with this project.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
+import java.util.Random;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Random random = new Random();
+		int value = random.nextInt(21);
+		
+		if (value % 2 ==0 ) {
+			System.out.println("This number is even! "+ value);
+		}
+		else {
+			System.out.println("This number is odd! " + value);
+		}
 
 	}
 
